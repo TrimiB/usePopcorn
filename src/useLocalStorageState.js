454191b8509b3ea35a04a27c008f223a36a546aa
Kushtrim <bujupikushtrim@outlook.com>
2024-05-04 20:57:41 +0200

    param renaming

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -4,13 +4,13 @@ import { useState, useEffect } from 'react';
  * A custom React hook that manages state in the browser's local storage.
  *
  * @param {string} key - The key to use for storing the state in local storage.
- * @param {any} defaultValue - The default value to use if the state is not found in local storage.
+ * @param {any} initialState - The default value to use if the state is not found in local storage.
  * @returns {[any, function(any): void]} - An array containing the current state value and a function to update the state.
  */
-export function useLocalStorageState(key, defaultValue) {
+export function useLocalStorageState(key, initialState) {
   const [value, setValue] = useState(function () {
     const storedWatched = localStorage.getItem(key);
-    return storedWatched ? JSON.parse(storedWatched) : defaultValue;
+    return storedWatched ? JSON.parse(storedWatched) : initialState;
   });
 
   useEffect(
